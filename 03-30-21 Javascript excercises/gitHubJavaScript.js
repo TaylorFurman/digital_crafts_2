a9d591a630995f046db4adc43c7a42bbcdf010c1
Taylor Furman <TFurman47@gmail.com>
2021-04-01 06:55:15 -0500

    update

--- 03-30-21 Javascript excercises/gitHubJavaScript.js
+++ 03-30-21 Javascript excercises/gitHubJavaScript.js
@@ -1,4 +1,4 @@
-function madlib(name, subject){
+function madlib(name, subject) {
     name = "Taylor";
     subject = "Science";
     return name + "'s favorite subject in school is " + subject;
@@ -8,18 +8,18 @@ function madlib(name, subject){
 console.log(madlib());
 
 
-function tipAmount(bill, service){
-    if (service == 'good'){
+function tipAmount(bill, service) {
+    if (service == 'good') {
         rate = 0.20;
         tip = rate * bill;
         console.log("You did a " + service + " job!  Here is your tip of $" + tip);
     }
-    else if (service == 'fair'){
+    else if (service == 'fair') {
         rate = 0.15;
         tip = rate * bill;
         console.log("You did a " + service + " job!  3.6 Not great not terrible...Here is your tip of $" + tip);
     }
-    else if (service == 'bad'){
+    else if (service == 'bad') {
         rate = 0.10;
         tip = rate * bill;
         console.log("You did a " + service + " job!  Time to find a new career...Here is your tip of $" + tip);
@@ -28,20 +28,20 @@ function tipAmount(bill, service){
 
 tipAmount(100, 'bad');
 
-function totalAmount(bill, service){
-    if (service == 'good'){
+function totalAmount(bill, service) {
+    if (service == 'good') {
         rate = 0.20;
         tip = rate * bill;
         total = tip + bill;
         console.log("You did a " + service + " job!  Here is your total of $" + total);
     }
-    else if (service == 'fair'){
+    else if (service == 'fair') {
         rate = 0.15;
         tip = rate * bill;
         total = tip + bill;
         console.log("You did a " + service + " job!  3.6 Not great not terrible...Here is your total of $" + total);
     }
-    else if (service == 'bad'){
+    else if (service == 'bad') {
         rate = 0.10;
         tip = rate * bill;
         total = tip + bill;
@@ -51,6 +51,6 @@ function totalAmount(bill, service){
 
 totalAmount(100, 'fair');
 
-function printNumbers(n, m){
-    when 
+function printNumbers(n, m) {
+    when
 }
