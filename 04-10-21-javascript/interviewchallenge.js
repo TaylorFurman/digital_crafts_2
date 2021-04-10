97bd1028ba48dcb47c6e7f95af70c9dcd9e903ba
unknown <TFurman47@gmail.com>
2021-04-10 12:30:22 -0500

    saturday update

---
+++ 04-10-21-javascript/interviewchallenge.js
@@ -0,0 +1,34 @@
+function fizzNumbers(startNumber, endNumber){
+    var i;
+    for(i = startNumber; i <= endNumber; i++){
+        if(i %3){
+            console.log("Fizz");
+        } 
+        else if (i %5){
+            console.log("Buzz");
+        }
+        else if (i %3 && 1 %5){
+            console.log("FizzBuzz");
+        }
+        else{
+            console.log(i);
+        }
+    }
+    
+}
+
+//fizzNumbers(1,100);
+
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+function naturalNumbers(x){
+    var i = x;
+    for (i =0; i<1000; i++){
+        if (i < 1000 && i % 3 ==0 || i % 5 ==0){
+            console.log(i);
+        }
+    }
+}
+naturalNumbers()
